e0728be638a303e7f22ba27e15a67ec68333b4c1
spaceplasma <chadbryantphd@gmail.com>
2017-11-23 19:28:30 -0700

    Add files via upload

--- iceberg_cnn_simple.py
+++ iceberg_cnn_simple.py
@@ -13,61 +13,117 @@ np.random.seed(1337)
 
 #import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Lambda#, Activation#, Input
-from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D#, GlobalAveragePooling2D
+from keras.layers import Dense, Dropout, Flatten, Lambda, Activation#, Input
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard, ReduceLROnPlateau
 from keras.layers.normalization import BatchNormalization
 from keras.optimizers import Adam
 #from keras.utils import np_utils
-from sklearn.ensemble import RandomForestClassifier
+#from sklearn.ensemble import RandomForestClassifier
 
 
 INPUT_PATH='C:\\Users\\bryacha\\projects\\Kaggle\\Iceberg\\data\\processed\\' 
 
-def get_model_all_CNN(bd):
-    ## Build model
-    model = Sequential()
+#def get_model_all_CNN(bd):
+#    ## Build model
+#    model = Sequential()
+#
+#    model.add(Lambda(lambda x: x, input_shape=(75, 75, bd)))
+#    
+#    model.add(ZeroPadding2D((2, 2)))
+#    model.add(Conv2D(32, (5,5), activation="relu", padding="valid"))
+#    model.add(BatchNormalization(axis=-1))
+#    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+##    model.add(Dropout(0.2))
+#    
+#    model.add(ZeroPadding2D((2, 2)))
+#    model.add(Conv2D(64, (3,3), activation="relu", padding="valid"))
+#    model.add(BatchNormalization(axis=-1))
+#    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+##    model.add(Dropout(0.2))
+#
+##    model.add(ZeroPadding2D((1, 1)))
+#    model.add(Conv2D(128, (3,3), activation="relu", padding="valid"))
+#    model.add(BatchNormalization(axis=-1))
+#    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+##    model.add(Dropout(0.2))
+#
+#    model.add(ZeroPadding2D((1, 1)))
+#    model.add(Conv2D(128, (3,3), activation="relu", padding="valid"))
+#    model.add(BatchNormalization(axis=-1))
+#    model.add(MaxPooling2D(2,2))
+##    model.add(Dropout(0.2))
+#
+#    model.add(ZeroPadding2D((1, 1)))
+#    model.add(Conv2D(2, (3, 3), activation='relu'))
+#    model.add(GlobalAveragePooling2D())
+#    
+##    model.add(Flatten())
+#
+##    model.add(Dense(64, activation='relu'))
+##    model.add(Dropout(0.2))
+#
+#    model.add(Dense(1, activation="sigmoid"))
+#    optimizer = Adam(lr=0.0003, decay=0.1)
+#    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+#
+#    return model
 
-    model.add(Lambda(lambda x: x, input_shape=(75, 75, bd)))
+def getModel_FCN():
+    #Building the model
+    model=Sequential()
     
-    model.add(ZeroPadding2D((2, 2)))
-    model.add(Conv2D(32, (5,5), activation="relu", padding="valid"))
-    model.add(BatchNormalization(axis=-1))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-#    model.add(Dropout(0.2))
+    model.add(Lambda(lambda x: x, input_shape=(75, 75, 3)))
     
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Conv2D(64, (3,3), activation="relu", padding="valid"))
-    model.add(BatchNormalization(axis=-1))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    
+    #Conv Layer 1
+    model.add(Conv2D(64, kernel_size=(3, 3),activation='relu'))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
     model.add(Dropout(0.2))
 
-#    model.add(ZeroPadding2D((1, 1)))
-    model.add(Conv2D(128, (3,3), activation="relu", padding="valid"))
-    model.add(BatchNormalization(axis=-1))
-#    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-#    model.add(Dropout(0.2))
+    #Conv Layer 2
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu' ))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.2))
 
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Conv2D(128, (3,3), activation="relu", padding="valid"))
-    model.add(BatchNormalization(axis=-1))
-#    model.add(MaxPooling2D(2,2))
-#    model.add(Dropout(0.2))
-    
+    #Conv Layer 3
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.2))
+
+    #Conv Layer 4
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.2))
+
+    #Flatten the data for upcoming dense layers
     model.add(Flatten())
 
-    model.add(Dense(64, activation='relu'))
+    #Dense Layers
+    model.add(Dense(512))
+    model.add(Activation('relu'))
     model.add(Dropout(0.2))
 
-    model.add(Dense(1, activation="sigmoid"))
-    optimizer = Adam(lr=0.0003, decay=0.1)
-    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    #Dense Layer 2
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.2))
 
+    #Sigmoid Layer
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    mypotim=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    model.compile(loss='binary_crossentropy',
+                  optimizer=mypotim,
+                  metrics=['accuracy'])
+    
     return model
 
 def get_scaled_imgs(df):
     imgs = []
-    R = []
+#    R = []
     
     for i, row in df.iterrows():
         #make 75x75 image
@@ -80,13 +136,13 @@ def get_scaled_imgs(df):
         b = (band_2 - band_2.mean()) / (band_2.max() - band_2.min())
         c = (band_3 - band_3.mean()) / (band_3.max() - band_3.min())
         
-        R0 = get_R(band_1,band_2,band_3)
-        
-        R.append(R0)
+#        R0 = get_R(band_1,band_2,band_3)
+#        
+#        R.append(R0)
 
         imgs.append(np.dstack((a, b, c)))
 
-    return np.array(imgs), R
+    return np.array(imgs)#, R
 
 def get_R(a,b,c):
        
@@ -145,6 +201,23 @@ def get_more_images(imgs):
     
     return more_images
 
+#def get_filtered_imgs(imgs):
+#    
+#    fimgs = []
+#    
+#    for i in range(0,imgs.shape[0]):
+#        a=imgs[i,:,:,0]
+#        b=imgs[i,:,:,1]
+#        c=imgs[i,:,:,2]
+#        
+#        af = cv2.bilateralFilter(a,5,15,15)
+#        bf = cv2.bilateralFilter(b,5,15,15)
+#        cf = cv2.bilateralFilter(c,5,15,15)
+#        
+#        fimgs.append(np.dstack((af,bf,cf)))
+#        
+#    return np.array(fimgs)
+
 #def random_forest_classifier(features, target):
 #    """
 #    To train the random forest classifier with features and target data
@@ -158,19 +231,28 @@ def get_more_images(imgs):
 
 # Read the json files into a pandas dataframe
 print('Fetching Training Data...')
-with open(INPUT_PATH + 'train.json') as datafile:
-    data = json.load(datafile)
+#with open(INPUT_PATH + 'train.json') as datafile:
+#    data = json.load(datafile)
     
-df_train = pd.DataFrame(data)
+df_train = pd.read_json(INPUT_PATH + 'train.json')    
+    
+#df_train = pd.DataFrame(data)
+
 df_train.inc_angle = df_train.inc_angle.replace('na',0)
 idx_tr = np.where(df_train.inc_angle>0)
 Ytrain = np.array(df_train['is_iceberg'])
-train_t = get_scaled_imgs(df_train)
-Xtrain = train_t[0]
-R_train = train_t[1]
+Xtrain = get_scaled_imgs(df_train)
+
+#train_t = get_scaled_imgs(df_train)
+#Xtrain = train_t[0]
+#R_train = train_t[1]
 
 Ytrain = Ytrain[idx_tr[0]]
-Xtrain = Xtrain[idx_tr[0],:,:,:]
+Xtrain = Xtrain[idx_tr[0],...]
+
+# Bilateral filter the images
+#Xtrain = get_filtered_imgs(Xtrain)
+
 #inc_angle_train = np.array(df_train['inc_angle'])
 
 #add more images to train on
@@ -180,36 +262,44 @@ Ytr_more = np.concatenate((Ytrain,Ytrain,Ytrain))
 batch_size = 32
 bd=3
 
-model = get_model_all_CNN(bd)
+#model = get_model_all_CNN(bd)
+model = getModel_FCN()
 model.summary()
 
-earlyStopping = EarlyStopping(monitor='val_loss', patience=20, verbose=0, mode='min')
-mcp_save = ModelCheckpoint('.mdl_wtsA.hdf5', save_best_only=True, monitor='val_loss', mode='min')
+earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
+mcp_save = ModelCheckpoint('.mdl_wtsFCN.hdf5', save_best_only=True, monitor='val_loss', mode='min')
 tensorboard = TensorBoard(log_dir='../logs', histogram_freq=0, write_graph=True, write_images=False)
-reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=1, epsilon=1e-4, mode='min')
-model.fit(Xtr_more[:,:,:,0:], Ytr_more, batch_size=batch_size, epochs=100, verbose=1, callbacks=[earlyStopping, mcp_save,tensorboard,reduce_lr_loss], validation_split=0.25)
+reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, verbose=1, epsilon=1e-4, mode='min')
+model.fit(Xtr_more[:,:,:,0:], Ytr_more, batch_size=batch_size, epochs=50, verbose=1, callbacks=[earlyStopping, mcp_save,tensorboard,reduce_lr_loss], validation_split=0.25)
 
-model.load_weights(filepath = '.mdl_wtsA.hdf5')
+model.load_weights(filepath = '.mdl_wtsFCN.hdf5')
 
 score = model.evaluate(Xtrain[:,:,:,0:], Ytrain, verbose=2)
 print('Train score:', score[0])
 print('Train accuracy:', score[1])
 
 print('Fetching Test Data...')
-with open(INPUT_PATH + 'test.json') as datafile:
-    data = json.load(datafile)
+#with open(INPUT_PATH + 'test.json') as datafile:
+#    data = json.load(datafile)
     
-df_test = pd.DataFrame(data)
+df_test = pd.read_json(INPUT_PATH + 'test.json')
+    
+#df_test = pd.DataFrame(data)
 df_test.inc_angle = df_test.inc_angle.replace('na',0)
-test_t = (get_scaled_imgs(df_test))
-Xtest = test_t[0]
-R_test = test_t[1]
+Xtest = (get_scaled_imgs(df_test))
+#Xtest = test_t[0]
+#R_test = test_t[1]
 
 predA_train = model.predict(Xtrain[:,:,:,0:])
 print(predA_train.reshape(predA_train.shape[0]))
 
 predA_test = model.predict(Xtest[:,:,:,0:])
 print(predA_test.reshape(predA_test.shape[0]))
+
+#set predicted values where inc_angle is 0 to 0 (not iceberg)
+#inc_angle_train = np.array(df_train['inc_angle'])
+#idx_test = np.where(inc_angle_train==0)
+#predA_test[idx_test[0]] = 0
 
 #trRF = np.hstack((predHH_train, x_inc_ang[:,np.newaxis]))
 ##trRF = np.hstack((predC_train,trRF))
@@ -225,7 +315,7 @@ print(predA_test.reshape(predA_test.shape[0]))
 submission = pd.DataFrame({'id': df_test["id"], 'is_iceberg': predA_test.reshape((predA_test.shape[0]))})
 print(submission.head(10))
 
-submission.to_csv(INPUT_PATH + '20171121_submission_4.csv', index=False)
+submission.to_csv(INPUT_PATH + '20171123_submission_4.csv', index=False)
 ### ----
 
 
